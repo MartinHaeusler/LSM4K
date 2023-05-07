2904861b8ba53292687abe683b2b1a897fcffafe
Martin Haeusler <martin.haeusler@student.uibk.ac.at>
2023-05-07 14:26:15 +0200

    more tests

--- src/test/kotlin/org/chronos/chronostore/test/cases/api/StoreManagementTest.kt
+++ src/test/kotlin/org/chronos/chronostore/test/cases/api/StoreManagementTest.kt
@@ -51,8 +51,46 @@ class StoreManagementTest {
     }
 
     @ChronoStoreTest
-    fun canCreateStoreAndIterateWithCursor() {
+    fun canCreateStoresAndPerformPutAndGetInSameTransaction(mode: ChronoStoreMode) {
 
+        fun performAssertions(tx: ChronoStoreTransaction) {
+            expectThat(tx) {
+                get { allStores }.hasSize(2).and {
+                    any {
+                        get { this.store.name }.isEqualTo("test")
+                        get { this.getLatest("foo") }.isEqualTo(Bytes("bar"))
+                        get { this.getLatest("bullshit") }.isNull()
+                    }
+                    any {
+                        get { this.store.name }.isEqualTo("math")
+                        get { this.getLatest("pi") }.isEqualTo(Bytes("3.1415"))
+                        get { this.getLatest("e") }.isEqualTo(Bytes("2.718"))
+                    }
+                }
+            }
+        }
+
+        mode.withChronoStore { chronoStore ->
+            chronoStore.transaction { tx ->
+                val test = tx.createNewStore("test", versioned = true)
+                test.put("foo", "bar")
+                val math = tx.createNewStore("math", versioned = false)
+                math.put("pi", "3.1415")
+                math.put("e", "2.718")
+
+                performAssertions(tx)
+
+                tx.commit()
+            }
+            chronoStore.transaction { tx ->
+                performAssertions(tx)
+            }
+        }
+    }
+
+    @ChronoStoreTest
+    fun canCreateStoreAndIterateWithCursor() {
+        TODO("implement this test!")
     }
 
 }
